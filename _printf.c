e3791e5a139a1dd19b338bbbd18c64505bf7e4ac
Pibo Shongwe <piboman@yahoo.com>
2023-08-19 15:46:51 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,35 +4,39 @@
 
 /**
  * print_char - Prints a character and increments the count.
- * @args: Variable argument list containing the character to be printed.
+ * @args: Variable argument list containing
+ * the character to be printed.
  * @count: Pointer to the counter tracking printed characters.
  * Return: Updated count of characters printed.
  */
-int print_char(va_list args, int *count) {
-    char c = va_arg(args, int);
-    
-    *count += _putchar(c);
-    return *count;
+int print_char(va_list args, int *count)
+{
+	char c = va_arg(args, int);
+	
+	*count += _putchar(c);
+	return (*count);
 }
 
 /**
  * print_string - Prints a string and increments the count.
- * @args: Variable argument list containing the string to be printed.
+ * @args: Variable argument list containing
+ * the string to be printed.
  * @count: Pointer to the counter tracking printed characters.
  * Return: Updated count of characters printed.
  */
-int print_string(va_list args, int *count) {
-    char *s = va_arg(args, char *);
-    int j;
-
-    if (s == NULL) {
-        s = "(null)";
-    }
-    for (j = 0; s[j] != '\0'; j++) {
-        _putchar(s[j]);
-        (*count)++;
-    }
-    return *count;
+int print_string(va_list args, int *count)
+{
+	char *s = va_arg(args, char *);
+	int j;
+	
+	if (s == NULL)
+		s = "(null)";
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		_putchar(s[j]);
+		(*count)++;
+	}
+	return (*count);
 }
 
 /**
@@ -44,38 +48,40 @@ int print_string(va_list args, int *count) {
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, count = 0;
-    
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
+	va_list args;
+	int i, count = 0;
+	
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-            i++;
-            switch (format[i]) {
-                case 'c':
-                    count = print_char(args, &count);
-                    break;
-                case 's':
-                    count = print_string(args, &count);
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(format[i]);
-                    count += 2;
-                    break;
-            }
-        } else {
-            _putchar(format[i]);
-            count++;
-        }
-    }
-
-    va_end(args);
-    return (count);
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count = print_char(args, &count);
+					break;
+				case 's':
+					count = print_string(args, &count);
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
