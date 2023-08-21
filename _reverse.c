75c561ea067ea50e4be06984d02170fb77559592
Pibo Shongwe <piboman@yahoo.com>
2023-08-22 00:23:31 +0200

    Update _reverse.c

--- _reverse.c
+++ _reverse.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * print_reverse - Prints a string in reverse order using putchar
- * @args: Variable argument list containing additional arguments
- * Return: Updated count of characters printed.
- */
+* print_reverse - Prints a string in reverse order using putchar
+* @args: Variable argument list containing additional arguments
+* Return: Updated count of characters printed.
+*/
 int print_reverse(va_list args)
 {
-    char *input = va_arg(args, char *);
-    int i, length, count;
+char *input = va_arg(args, char *);
+int i, length, count;
 
-    length = _strlen(input);
-    count = 0;
+length = _strlen(input);
+count = 0;
 
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(input[i]);
-        count++;
-    }
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(input[i]);
+count++;
+}
 
-    return count;
+return count;
 }
 
 /**
- * _strlen - calculates length of a string
- * @s: pointer to string
- *
- * Return: int length
- */
+* _strlen - calculates length of a string
+* @s: pointer to string
+*
+* Return: int length
+*/
 int _strlen(char *s)
 {
-	int length;
+int length;
 
-	length = 0;
-	while (s[length] != '\0')
-		length++;
-	return (length);
+length = 0;
+while (s[length] != '\0')
+length++;
+return (length);
 }
