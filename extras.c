10f810b981881b883eedc8fbf761b6dc7f456cc9
Pibo Shongwe <piboman@yahoo.com>
2023-08-23 01:02:40 +0200

    Update extras.c

--- extras.c
+++ extras.c
@@ -24,3 +24,36 @@ _putchar('%');
 _putchar(c);
 return (2);
 }
+
+/**
+* handle_pointer - Handles printing of pointer addresses.
+* @args: Variable argument list containing the pointer to be printed.
+* Return: The number of characters printed.
+*/
+int handle_pointer(va_list args)
+{
+void *ptr;
+char hex_digits[] = "0123456789abcdef";
+char buffer[16];
+int i, count;
+
+ptr = va_arg(args, void *);  
+count = 0;
+_putchar('0');
+_putchar('x');
+
+for (i = sizeof(void *) * 2 - 1; i >= 0; i--)
+{
+buffer[i] = hex_digits[(uintptr_t)ptr & 0xf];
+ptr = (void *)((uintptr_t)ptr >> 4);
+}
+
+for (i = 0; i < sizeof(void *) * 2; i++)
+{
+_putchar(buffer[i]);
+count++;
+}
+
+return (count);
+}
+
