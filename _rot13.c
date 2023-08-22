bd06877447f7f9b86e889d1333dd491839f80064
Pibo Shongwe <piboman@yahoo.com>
2023-08-23 00:01:22 +0200

    Create _rot13.c

---
+++ _rot13.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strchr - finds a char in a string
+* @s: string
+* @c: char
+* Return: pointer to char in string
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+
+while (1)
+{
+i = *s++;
+
+if (i == c)
+return (s - 1);
+
+if (i == 0)
+
+return (NULL);
+}
+}
+
+/**
+* rot13 - encode string using rot13
+* @str: input string
+* Return: result string
+*/
+char *rot13(char *str)
+{
+char *found;
+char *input;
+char *output;
+int i;
+
+input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; str[i] != '\0'; i++)
+{
+found = _strchr(input, str[i]);
+if (found)
+{
+str[i] = output[found - input];
+}
+}
+return (str);
+}
