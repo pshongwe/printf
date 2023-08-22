461d04ba81479f88d7605be2c10d2aa241880068
Pibo Shongwe <piboman@yahoo.com>
2023-08-23 01:37:08 +0200

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -10,5 +10,28 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+static char buffer[BUFSIZE];
+static int buffer_index, count;
+
+buffer_index = 0;
+count = 0;
+
+if (c == '\0')
+{
+write(1, buffer, buffer_index);
+buffer_index = 0;
+}
+else
+{
+buffer[buffer_index++] = c;
+count++;
+
+if (buffer_index >= BUFSIZE - 1) // Leave room for null terminator
+{
+write(1, buffer, buffer_index);
+buffer_index = 0;
+}
+}
+
+return (count);
 }
